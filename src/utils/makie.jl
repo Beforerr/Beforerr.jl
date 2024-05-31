c8ef69353f75c723176498e2734fc6cfb415538a
Beforerr <zzj956959688@gmail.com>
2024-05-31 00:25:49 -0700

    refactor

--- src/utils/makie.jl
+++ src/utils/makie.jl
@@ -1,7 +1,7 @@
 function add_label!(layout, label; position=TopLeft())
     Label(layout[1, 1, position], label,
         font=:bold,
-        padding = (0, 40, 0, 0),
+        padding=(0, 40, 0, 0),
         halign=:right)
 end
 
@@ -12,7 +12,7 @@ add labels to a grid of layouts
 # Notes
 - See `tag_facet` in `egg` for reference
 """
-function add_labels!(layouts; labels = ('a':'z'), open = "(", close = ")")
+function add_labels!(layouts; labels=('a':'z'), open="(", close=")")
     for (label, layout) in zip(labels, layouts)
         tag = open * label * close
         add_label!(layout, tag)
@@ -23,19 +23,15 @@ function pretty_legend!(fig, grid)
     legend!(fig[0, 1:end], grid, titleposition=:left, orientation=:horizontal)
 end
 
-function easy_save(name, fig; dir="figures")
-    path = "$dir/$name"
-    mkpath(dir)
+function easy_save(name, fig; formats=[:pdf, :png], dir="figures", log=true)
+    path = joinpath(dir, name) |> mkpath
 
-    save("$path.png", fig, px_per_unit=4)
-    save("$path.pdf", fig)
-    
-    # log the path saved
-    @info "Saved $(abspath(path)).png"
-    fig
+    for format in formats
+        save("$path.$format", fig; px_per_unit=4)
+        log && @info "Saved $(abspath("$path.$format"))"
+    end
+
+    return fig
 end
 
-function easy_save(name; dir="figures")
-    fig = current_figure()
-    easy_save(name, fig, dir=dir)
-end
+easy_save(name; kwargs...) = easy_save(name, current_figure(); kwargs...)
